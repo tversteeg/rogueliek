2190aee19af0cf57decd07e3f6b019a2e1e27e84
Thomas Versteeg <thomasversteeg@gmx.com>
2016-02-05 11:37:15 +0700

    Quicksort vertices for delaunay

--- src/rogueliek/delaunay.c
+++ src/rogueliek/delaunay.c
@@ -1,6 +1,9 @@
 #include "delaunay.h"
 
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define SORT_MAX_LEVELS 64
 
 typedef struct {
 	float x, y;
@@ -17,24 +20,83 @@ static int l_delaunayTriangulate(lua_State *lua)
 		exit(1);
 	}
 
-	float *x = (float*)malloc(len * sizeof(float));
-	float *y = (float*)malloc(len * sizeof(float));
+	len--;
+	float *xs = (float*)malloc(len * sizeof(float));
+	float *ys = (float*)malloc(len * sizeof(float));
 
-	for(int i = 1; i < len; i++){
+	for(int i = 1; i < len + 1; i++){
 		lua_pushinteger(lua, i);
 		lua_gettable(lua, 1);
-		x[i] = lua_tonumber(lua, -1);
+		xs[i - 1] = lua_tonumber(lua, -1);
 
 		lua_pushinteger(lua, i);
 		lua_gettable(lua, 2);
-		y[i] = lua_tonumber(lua, -1);
+		ys[i - 1] = lua_tonumber(lua, -1);
 	}
+
+	int *ids = NULL;
+	delaunayTriangulate(&ids, xs, ys, len);
+
+	free(xs);
+	free(ys);
+
 	return 0;
 }
 
-static void sortPositions(_vert **vs)
+static bool sortFunc(_vert v1, _vert v2)
 {
+	if(v1.x < v2.x){
+		return true;
+	}else if(v1.x == v2.x){
+		return v1.y < v2.y;
+	}
+	return false;
+}
 
+static void swapInt(int *x, int *y){
+	*x ^= *y;
+	*y ^= *x;
+	*x ^= *y;
+}
+
+// Quicksort
+static void sortPositions(_vert *vs, int amount)
+{
+	int beg[SORT_MAX_LEVELS], end[SORT_MAX_LEVELS];
+
+	beg[0] = 0;
+	end[0] = amount;
+	for(int i = 0; i >= 0;){
+		int l = beg[i];
+		int r = end[i] - 1;
+		if(l < r){
+			_vert piv = vs[l];
+			while(l < r){
+				while(sortFunc(piv, vs[r]) && l < r){
+					r--;
+				}
+				if(l < r){
+					vs[l++] = vs[r];
+				}
+				while(!sortFunc(piv, vs[l]) && l < r){
+					l++;
+				}
+				if(l < r){
+					vs[r--] = vs[l];
+				}
+			}
+			vs[l] = piv;
+			beg[i + 1] = l + 1;
+			end[i + 1] = end[i];
+			end[i++] = l;
+			if(end[i] - beg[i] > end[i - 1] - beg[i - 1]){
+				swapInt(beg + i, beg + i - 1);
+				swapInt(end + i, end + i - 1);
+			}
+		}else{
+			i--;
+		}
+	}
 }
 
 void delaunayRegisterLua(lua_State *lua)
@@ -48,9 +110,16 @@ int delaunayTriangulate(int **ids, const float *x, const float *y, int amount)
 	for(int i = 0; i < amount; i++){
 		vs[i].x = x[i];
 		vs[i].y = y[i];
+		printf("(%.f, %.f) ", vs[i].x, vs[i].y);
 	}
 
-	sortPositions(&vs);
+	sortPositions(vs, amount);
+	printf("\n");
+
+	for(int i = 0; i < amount; i++){
+		printf("(%.f, %.f) ", vs[i].x, vs[i].y);
+	}
+	printf("\n\n");
 
 	return 0;
 }
